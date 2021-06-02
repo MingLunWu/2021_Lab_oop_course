43dab02689b4c8e6753448b9e4c0a177c21f79ad
MingLun Wu <allen6997535@gmail.com>
2021-06-02 15:36:58 +0800

    update answer

---
+++ Week3/Tokenizer_ans.py
@@ -0,0 +1,43 @@
+class Tokenizer:
+    def __init__(self) -> None:
+        self.dictionary = {
+            "I": 0,
+            "am": 1,
+            "a": 2,
+            "good": 3,
+            "boy": 4,
+            "how": 5,
+            "about": 6,
+            "you": 7,
+            "[UNK]": 8
+        }
+    
+    def encode(self, text):
+        result = list()
+        for word in text:
+            result.append(self.dictionary.get(word, 8)) # 8 is unknown.
+        return result
+    
+    def decode(self, indexes):
+        result = list()
+        reverse_dictionary = {v:k for (k,v) in self.dictionary.items()} # List Comprehension
+        for idx in indexes:
+            result.append(reverse_dictionary.get(idx, '[UNK]'))
+        return result
+
+
+    
+if __name__ == "__main__":
+    tokenizer = Tokenizer()
+    # Test Case 1
+    indexes = tokenizer.encode(["I", "am", "a", "good", "boy"])
+    print(indexes) # 答案應該要是 [0, 1, 2, 3, 4]
+    words = tokenizer.decode(indexes) # 答案應該要是 ['I', 'am', 'a', 'good', 'boy']
+    print(words)
+
+    # Test Case 2
+    indexes = tokenizer.encode(["Allen", "how", "about", "you", "?"])
+    print(indexes) # 答案應該要是 [8, 5, 6, 7, 8]
+    words = tokenizer.decode(indexes)
+    print(words) # 答案應該要是['[UNK]', 'how', 'about', 'you', '[UNK]']
+
